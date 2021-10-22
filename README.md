7f0e6ff8c3dd031225ea109385a8c5ccf0a246b4
Franklin <fr@nkl.in>
2021-10-22 22:51:50 +0200

    Breaking: Modernized everything
    - Exports a class
    - Object arguments
    - Promises
    - Removed callbacks
    - Removed sleep get/set method
    - Renamed methods
    - Removed obsolete stuff

--- README.md
+++ README.md
@@ -1,62 +1,43 @@
-brothermfc
-==========
+# brothermfc
 
 Access Brother MFC web UI details with node.js
 
 [![npm](https://img.shields.io/npm/v/brothermfc.svg?maxAge=3600)](https://github.com/fvdm/nodejs-brothermfc/blob/master/CHANGELOG.md)
-[![Build Status](https://travis-ci.org/fvdm/nodejs-brothermfc.svg?branch=master)](https://travis-ci.org/fvdm/nodejs-brothermfc)
-[![Dependency Status](https://gemnasium.com/badges/github.com/fvdm/nodejs-brothermfc.svg)](https://gemnasium.com/github.com/fvdm/nodejs-brothermfc#runtime-dependencies)
+[![Build Status](https://github.com/fvdm/nodejs-brothermfc/actions/workflows/node.js.yml/badge.svg?branch=master)](https://github.com/fvdm/nodejs-brothermfc/actions/workflows/node.js.yml)
 [![Coverage Status](https://coveralls.io/repos/github/fvdm/nodejs-brothermfc/badge.svg?branch=master)](https://coveralls.io/github/fvdm/nodejs-brothermfc?branch=master)
 
 
-Example
--------
+## Usage
 
 ```js
-var brother = require ('brothermfc') ({
-  hostname: 'myprinter.lan'
+const BrotherMFC = require ('brothermfc');
+const brother = new BrotherMFC ({
+  url: 'http://myprinter.lan',
 });
 
-brother.current (function (err, data) {
-  console.log (err || data);
-});
+brother.current()
+  .then (console.log)
+  .catch (console.error)
+;
 ```
 
 
-Installation
-------------
+## Installation
 
-Normal: `npm install brothermfc`
-
-Development: `npm install fvdm/nodejs-brothermfc`
+`npm install brothermfc`
 
 
-Configuration
--------------
+## Configuration
 
-param    | type   | required | default | description
-:--------|:-------|:---------|:--------|:------------------------------------------
-hostname | string | yes      |         | Printer hostname or IP
-port     | number | no       | 80      | Printer web UI port
-protocol | string | no       | http    | Printer web UI protocol, `http` or `https`
-prefix   | string | no       |         | Printer web UI prefix
-ippPort  | number | no       | 631     | Printer IPP service port
-timeout  | number | no       | 5000    | Web require time out in ms, 1000 = 1 sec
+param     | type   | default | description
+:---------|:-------|:--------|:------------------------------------------
+url       | string |         | Base URL, ie. `http://printer:1234`
+[ippPort] | number | 631     | IPP service port
+[timeout] | number | 5000    | Request time out in ms
 
 
-#### Example
-
-```js
-var brother = require ('brothermfc') ({
-  hostname: 'printer.lan',
-  protocol: 'https'
-});
-```
-
-
-current
--------
-**( callback )**
+## current
+**( )**
 
 Get current information, like display text and ink levels, directly
 from the IPP interface. This is the most accurate information compared
@@ -65,21 +46,20 @@ to the `general.status` method which is a calculation.
 **Note:** this method may also work with other models and brands.
 
 
-param    | type     | required | description
-:--------|:---------|:---------|:-----------
-callback | function | yes      | see Usage.
-
-
-#### Example
+### Example
 
 ```js
-brother.current (callback);
+brother.current()
+  .then (console.log)
+  .catch (console.error)
+;
 ```
 
-#### Output
+### Output
 
 ```js
-{ state: 'idle',
+{
+  state: 'idle',
   stateReasons: 'none',
   jobs: 0,
   uptime: 613845,
@@ -101,128 +81,77 @@ stateReasons | more descriptive status message
 jobs         | print tasks running and queued
 uptime       | seconds since last boot
 uptimeDate   | `Date` object from `uptime`
-ink          | inklevels in %, colors depend in model
+ink          | inklevels in %, colors depend on model
 
 
-general.status
---------------
-**( callback )**
+## generalStatus
+**( )**
 
 Get basic status details, like display text and ink levels.
 
 **Note:** `ink` levels are less accurate then the `.current` method.
 
 
-param    | type     | required | description
-:--------|:---------|:---------|:-----------
-callback | function | yes      | see Usage.
-
-
-#### Example
+### Example
 
 ```js
-brother.general.status (callback);
+brother.generalStatus()
+  .then (console.log)
+  .catch (console.error)
+;
 ```
 
 
-#### Output
+### Output
 
 ```js
-{ model: 'MFC-J4710DW',
+{
+  model: 'MFC-J4710DW',
   status: 'moniOk',
   message: 'Gereed',
-  ink: { magenta: 100, cyan: 100, yellow: 100, black: 91 },
+  ink: {
+    magenta: 100,
+    cyan: 100,
+    yellow: 100,
+    black: 91
+  },
   contact: 'Franklin',
-  location: 'Here' }
+  location: 'Here'
+}
 ```
 
 
-general.information
--------------------
-**( callback )**
+## generalInformation
+**( )**
 
 Get information specific to the device, i.e. IP-address and serialnumber.
 
-param    | type     | required | description
-:--------|:---------|:---------|:-----------
-callback | function | yes      | see Usage.
 
 ```js
-brother.general.information (callback);
+brother.generalInformation()
+  .then (console.log)
+  .catch (console.error)
+;
 ```
 
 
-#### Output
+### Output
 
 ```js
-{ node_name: 'BRA123BC456DE78',
+{
+  node_name: 'BRA123BC456DE78',
   model_name: 'Brother MFC-J4710DW',
   location: 'Here',
   contact: 'Franklin',
   ip_address: '192.168.1.158',
   serial_no: 'A12345B678',
   firmware_version: 'H',
-  page_counter: 26 }
-```
-
-
-general.sleep
--------------
-**( [preset], callback )**
-
-Get or change the device's sleep timeout.
-
-param    | type     | required | description
-:--------|:---------|:---------|:-----------
-preset   | number   | no       | Preset number corresponding to minutes
-callback | function | yes      | see [Example](#example)
-
-
-### Get
-
-```js
-brother.general.sleep (callback);
-```
-
-
-#### Output
-
-```js
-{
-  presets: {
-    1: 2,
-    2: 3,
-    3: 5,
-    4: 10,
-    5: 30,
-    6: 60
-  ],
-  value: {
-    key: 4, minutes: 10
-  }
+  page_counter: 26
 }
 ```
 
-The `presets` property are the predefined minutes you can choose from.
-The `value` property is the current setting.
 
-
-### Change
-
-```js
-// set to key `5` = 30 minutes
-brother.general.sleep (5, callback);
-```
-
-
-#### Output
-
-* OK: `data` is `true` (boolean)
-* Fail: `err` is `Error: POST failed` with `err.error`
-
-
-License
--------
+## License
 
 This is free and unencumbered software released into the public domain.
 
@@ -247,12 +176,10 @@ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 
-For more information, please refer to <http://unlicense.org>
+For more information, please refer to <https://unlicense.org>
 
 
-Author
-------
+## Author
 
-[Franklin van de Meent](https://frankl.in)
-
-[![Buy me a coffee](https://frankl.in/u/kofi/kofi-readme.png)](https://ko-fi.com/franklin)
+[Franklin](https://fvdm.com)
+| [![Buy me a coffee](https://fvdm.com/donating)
--- brothermfc.js
+++ brothermfc.js
@@ -1,368 +1,196 @@
 /*
 Name:         brothermfc
 Description:  Access Brother MFC web UI details with node.js
-Author:       Franklin van de Meent (https://frankl.in)
-Feedback:     https://github.com/fvdm/nodejs-brothermfc/issues
+Author:       Franklin (https://fvdm.com)
 Source:       https://github.com/fvdm/nodejs-brothermfc
 License:      Unlicense / Public Domain (see LICENSE file)
 */
 
-var ipp = require ('ipp');
-var httpreq = require ('httpreq');
+const ipp = require ('ipp');
+const { doRequest } = require ('httpreq');
 
-var config = {
-  timeout: 5000,
-  protocol: 'http',
-  hostname: null,
-  port: 80,
-  prefix: '',
-  ippPort: 631
-};
+module.exports = class BrotherMFC {
 
+  /**
+   * @param   {object}  o
+   * @param   {string}  o.url             `http://printer.local:1234`
+   * @param   {number}  [o.timeout=5000]  Request timeout in ms
+   * @param   {number}  [o.ippPort=631]   Printer IPP port
+   */
 
-/**
- * Process response
- *
- * @callback callback
- * @param response {object} - httpreq response details
- * @param callback {function} - `function (err, data) {}`
- * @returns {void}
- */
+  constructor ({
+    url,
+    timeout = 5000,
+    ippPort = 631,
+  }) {
+    url = new URL (url);
 
-function doResponse (response, callback) {
-  var data = response && response.body || '';
-  var error = null;
-
-  var res = {
-    code: response.statusCode,
-    headers: response.headers,
-    data: data.trim ()
-  };
-
-  if (response.statusCode >= 300) {
-    error = new Error ('HTTP error');
-    error.code = res.code;
-    callback (error);
-    return;
-  }
-
-  callback (null, res);
-}
-
-
-/**
- * Communication
- *
- * @callback callback
- * @param props {object} - Request properties
- * @param props.path - Request path
- * @param [props.method = GET] {string} - HTTP method
- * @param [props.protocol = http] {string} - `https` or `http`
- * @param [props.hostname = config.hostname] {string} - Printer hostname
- * @param [props.port = 80] {number} - Printer web UI port
- * @param [props.headers] {object} - Additional request headers
- * @returns {void}
- */
-
-function talk (props, callback) {
-  var options = {
-    method: props.method || 'GET',
-    parameters: props.query || null,
-    timeout: config.timeout,
-    headers: {}
-  };
-
-  var port = props.protocol === 'https' ? 443 : 80;
-
-  options.url = props.protocol || config.protocol;
-  options.url += '://';
-  options.url += props.hostname || config.hostname;
-  options.url += ':';
-  options.url += props.port || config.port || port;
-  options.url += config.prefix + props.path;
-
-  // custom request headers
-  if (props.headers) {
-    options.headers = props.headers;
-  }
-
-  // request
-  httpreq.doRequest (options, function (err, res) {
-    if (err) {
-      callback (err);
-      return;
-    }
-
-    doResponse (res, callback);
-  });
-}
-
-
-/**
- * Method: general.status
- *
- * @callback callback
- * @param callback {function} - `function (err, data) {}`
- * @returns {void}
- */
-
-function methodGeneralStatus (callback) {
-  talk ({ path: '/general/status.html' }, function (err, res) {
-    var result = {
-      model: null,
-      status: null,
-      message: null,
-      ink: {}
+    this._config = {
+      url,
+      timeout,
+      ippPort,
     };
+  }
 
-    if (err) {
-      callback (err);
-      return;
-    }
 
+  /**
+   * Communication
+   *
+   * @param   {object}  o
+   * @param   {string}  o.path          Request path
+   * @param   {object}  [o.parameters]  Request parameters
+   * @param   {string}  [o.method=GET]  HTTP method
+   * @param   {object}  [o.headers]     Additional request headers
+   *
+   * @return  {Promise<mixed>}
+   */
+
+  async _talk ({
+    path,
+    parameters,
+    headers,
+    method = 'GET',
+  }) {
+    const options = {
+      url: `${this._config.url.protocol}//${this._config.url.host}`
+        + this._config.url.pathname + path
+      ,
+      method,
+      parameters,
+      timeout: this._config.timeout,
+      headers,
+    };
+  
+    return doRequest (options)
+      .then (res => res.body)
+    ;
+  }
+  
+  
+  /**
+   * Method: generalStatus
+   *
+   * @returns {Promise<object>}
+   */
+  
+  async generalStatus () {
+    const data = await this._talk ({ path: '/general/status.html' });
+    const result = {
+      ink: {},
+    };
+  
     res.data.replace (/<div id="modelName"><h1>([^<]+)<\/h1>/, function (s, t) {
       result.model = t;
     });
-
+  
     res.data.replace (/<div id="moni_data"><span class="moni ([^\"]+)">([^<]+)<\/span><\/div>/, function (s, t, m) {
       result.status = t;
       result.message = m;
     });
-
+  
     res.data.replace (/<img src="\.\.\/common\/images\/(\w+)\.gif" alt="([^\"]+)" class="tonerremain" height="(\d+)px"/g, function (s, c, s2, p) {
-      result.ink [c] = Math.round (p * 1.7857142857);
+      result.ink[c] = Math.round (p * 1.7857142857);
     });
-
+  
     res.data.replace (/<li class="(contact|location)">[^<]+<span class="spacer">:<\/span>([^<]+)<\/li>/g, function (s, t, v) {
-      result [t] = v;
+      result[t] = v;
     });
-
-    callback (null, result);
-  });
-}
-
-
-/**
- * Method: general.information
- *
- * @callback callback
- * @param callback {function} - `function (err, data) {}`
- * @returns {void}
- */
-
-function methodGeneralInformation (callback) {
-  talk ({ path: '/etc/mnt_info.csv' }, function (err, res) {
-    var data;
-    var one;
-    var two;
-    var result = {};
-    var i;
-    var val;
-    var key;
-
-    if (err) {
-      callback (err);
-      return;
-    }
-
+  
+    return result;
+  }
+  
+  
+  /**
+   * Method: generalInformation
+   *
+   * @returns {Promise<object>}
+   */
+  
+  async generalInformation () {
+    const data = await this._talk ({ path: '/etc/mnt_info.csv' });
+  
     data = res.data.replace (/\""{0}/g, '');
     data = data.split ('\n');
-
-    one = data [0] .split (',');
-    two = data [1] .split (',');
-    result = {};
-
-    for (i = 0; i < one.length; i++) {
-      val = two [i] .trim ();
-      key = one [i] .trim () .replace (/(.+)/, function (s, t) {
-        return t.toLowerCase () .replace (' ', '_') .replace ('.', '');
+  
+    const one = data[0].split (',');
+    const two = data[1].split (',');
+    const result = {};
+    let key;
+    let val;
+  
+    for (let i = 0; i < one.length; i++) {
+      val = two[i].trim();
+      key = one[i].trim().replace (/(.+)/, (s, t) => {
+        return t
+          .toLowerCase()
+          .replace (' ', '_')
+          .replace ('.', '')
+        ;
       });
-
+  
       if (key !== '' && val !== '') {
-        result [key] = val.match (/^\d+$/) ? val * 1 : val;
+        result[key] = val.match (/^\d+$/) ? val * 1 : val;
       }
+  
+      return result;
     }
-
-    callback (null, result);
-  });
-}
-
-
-/**
- * Method: sleep
- *
- * `set` values:
- * 0 = 1 min
- * 1 = 2 min
- * 2 = 3 min
- * 3 = 5 min
- * 4 = 10 min
- * 5 = 30 min
- * 6 = 60 min
- *
- * @callback callback
- * @param [set] {number} - 1-6, see above. Leave out to get current value.
- * @param callback {function} - `function (err, data) {}`
- * @returns {void}
- */
-
-function methodSleep (set, callback) {
-  var result = {
-    presets: {}
-  };
-
-  var form = {
-    pageid: 5,
-    postif_registration_reject: 1,
-    B15: set
-  };
-
-  if (!callback) {
-    callback = set;
-
-    talk ({ path: '/general/sleep.html' }, function (err, res) {
-      if (err) {
-        callback (err);
-        return;
-      }
-
-      res.data.replace (/<select id="B15" name="B15" >(.+)<\/select>/, function (s, sel) {
-        sel.replace (/<option value="(\d+)"( selected="selected")?>(\d+)&#32;Mins<\/option>/g, function (s2, a, b, c) {
-          result.presets [parseInt (a, 10)] = parseInt (c, 10);
-
-          if (b) {
-            result.value = {
-              key: parseInt (a, 10),
-              minutes: parseInt (c, 10)
-            };
-          }
-        });
+  }
+  
+  
+  /**
+   * Method: current
+   *
+   * @returns {Promise<object>}
+   */
+  
+  function current () {
+    const printer = ipp.Printer (
+      this._config.url.protocol + '//'
+      + this._config.url.ippPort
+      + '/ipp/printer'
+    );
+  
+    const msg = {
+      'operation-attributes-tag': {
+        'requested-attributes': [
+          'queued-job-count',
+          'marker-names',
+          'marker-levels',
+          'printer-state',
+          'printer-state-reasons',
+          'printer-up-time',
+        ],
+      },
+    };
+  
+    return new Promise ((resolve, reject) => {
+      printer.execute ('Get-Printer-Attributes', msg, (err, data) => {
+        if (err) {
+          reject (err);
+          return;
+        }
+  
+        data = data['printer-attributes-tag'] || {};
+  
+        const result = {
+          state: data['printer-state'] || null,
+          stateReasons: data['printer-state-reasons'] || null,
+          jobs: data['queued-job-count'] || 0,
+          uptime: data['printer-up-time'] || 0,
+          uptimeDate: new Date (Date.now() - (result.uptime * 1000)),
+          ink: {},
+        };
+  
+        if (Array.isArray (data['marker-names']) && Array.isArray (data['marker-levels'])) {
+          data['marker-names'].forEach ((name, i) => {
+            name = name.toLowerCase().replace (/.*\u001e(\w+) .*/, '$1');
+            result.ink[name] = data['marker-levels'][i];
+          });
+        }
+  
+        resolve (result);
       });
-
-      callback (null, result);
     });
-
-    return;
   }
-
-  talk (
-    {
-      method: 'POST',
-      path: '/general/sleep.html',
-      query: form
-    },
-    function (err, res) {
-      var error = null;
-
-      if (err) {
-        callback (err);
-        return;
-      }
-
-      if (!!~res.data.indexOf ('<div class="postSuccess">')) {
-        callback (null, true);
-        return;
-      }
-
-      error = new Error ('Unknown error');
-      error.statusCode = res.code;
-      error.body = res.data;
-
-      res.data.replace (/<div class="postError">([^<]+)<\/div>/, function (str, message) {
-        error = new Error ('POST failed');
-        error.statusCode = res.code;
-        error.error = message.replace ('&#32;', ' ');
-      });
-
-      callback (error);
-    }
-  );
-}
-
-
-/**
- * Method: current
- *
- * @callback callback
- * @param callback {function} - `function (err, data) {}`
- * @returns {void}
- */
-
-function methodCurrent (callback) {
-  var printer = ipp.Printer ('http://' + config.hostname + ':' + config.ippPort + '/ipp/printer');
-  var msg = {
-    'operation-attributes-tag': {
-      'requested-attributes': [
-        'queued-job-count',
-        'marker-names',
-        'marker-levels',
-        'printer-state',
-        'printer-state-reasons',
-        'printer-up-time'
-      ]
-    }
-  };
-
-  printer.execute ('Get-Printer-Attributes', msg, function (err, data) {
-    var error = null;
-    var result = {};
-    var name;
-    var i;
-
-    if (err) {
-      error = new Error ('ipp error');
-      error.error = err;
-      callback (err);
-      return;
-    }
-
-    data = data ['printer-attributes-tag'] || {};
-
-    result.state = data ['printer-state'] || null;
-    result.stateReasons = data ['printer-state-reasons'] || null;
-    result.jobs = data ['queued-job-count'] || 0;
-    result.uptime = data ['printer-up-time'] || 0;
-    result.uptimeDate = new Date (Date.now () - (result.uptime * 1000));
-
-    result.ink = {};
-
-    if (data ['marker-names'] instanceof Array && data ['marker-levels'] instanceof Array) {
-      for (i = 0; i < data ['marker-names'] .length; i++) {
-        name = data ['marker-names'] [i] .toLowerCase () .replace (/.*\u001e(\w+) .*/, '$1');
-        result.ink [name] = data ['marker-levels'] [i];
-      }
-    }
-
-    callback (null, result);
-  });
-}
-
-
-/**
- * Module interface
- *
- * @param setup {object} - Configuration parameters
- * @param setup.hostname {string} - Printer hostname or IP
- * @param [setup.port = 80] {number} - Printer UI port
- * @param [setup.ippPort = 631] {number} - Printer IPP port
- * @param [setup.prefix] {string} - Path prefix, for proxies
- * @param [setup.protocol = http] {string} - Printer UI protocol, `http` or `https`
- * @returns {object} - Methods
- */
-
-module.exports = function (setup) {
-  var i;
-
-  for (i in setup) {
-    config [i] = setup [i];
-  }
-
-  return {
-    current: methodCurrent,
-    sleep: methodSleep,
-    general: {
-      status: methodGeneralStatus,
-      information: methodGeneralInformation
-    }
-  };
+  
 };
